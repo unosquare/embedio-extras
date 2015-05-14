1018a9874b593616f3964cb443312d9378c946bc
Geo Perez <geovanni.perez@gmail.com>
2015-05-14 17:49:52 -0500

    Working on BearerToken

---
+++ Unosquare.Labs.EmbedIO.BearerToken/BearerToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unosquare.Labs.EmbedIO.BearerToken
+{
+    public class BearerToken
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ Unosquare.Labs.EmbedIO.BearerToken/BearerTokenModule.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Unosquare.Labs.EmbedIO.BearerToken
+{
+    public class BearerTokenModule : WebModuleBase
+    {
+        public delegate bool ValidateHandler(WebServer server, HttpListenerContext context, out Dictionary<string, object> payload);
+
+        public BearerTokenModule(ValidateHandler validation, string secretKey = "0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjbGF")
+        {
+            AddHandler("/token", HttpVerbs.Get, (server, context) =>
+            {
+                Dictionary<string, object> payload;
+
+                if (validation(server, context, out payload))
+                {
+                    if (payload == null) payload = new Dictionary<string, object>();
+
+                    payload.Add("EmbedIOAuthKey", 1);
+
+                    context.JsonResponse(new BearerToken
+                    {
+                        Token = JWT.JsonWebToken.Encode(payload, secretKey, JWT.JwtHashAlgorithm.HS256)
+                    });
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                }
+
+                return true;
+            });
+        }
+
+        public override string Name
+        {
+            get { return "Bearer Token Module"; }
+        }
+    }
+}
