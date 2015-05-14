04fcf13845cecbdc211670b05de7fa2c688ba48a
Geo Perez <geovanni.perez@gmail.com>
2015-05-14 17:08:07 -0500

    Improvements to JsonServer

--- Unosquare.Labs.EmbedIO.JsonServer/JsonServerModule.cs
+++ Unosquare.Labs.EmbedIO.JsonServer/JsonServerModule.cs
@@ -1,20 +1,32 @@
-﻿using System.Linq;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 
 namespace Unosquare.Labs.EmbedIO.JsonServer
 {
     public class JsonServerModule : WebModuleBase
     {
         public dynamic Data { get; set; }
+        public string JsonPath { get; set; }
 
         public JsonServerModule(string basePath = "/api/", string jsonPath = null)
         {
-            if (String.IsNullOrWhiteSpace(jsonPath) == false && File.Exists(jsonPath))
+            JsonPath = jsonPath;
+
+            if (String.IsNullOrWhiteSpace(jsonPath) == false)
             {
-                var jsonData = File.ReadAllText(jsonPath);
-                Data = JsonConvert.DeserializeObject(jsonData);
+                if (File.Exists(jsonPath))
+                {
+                    var jsonData = File.ReadAllText(jsonPath);
+                    Data = JsonConvert.DeserializeObject(jsonData);
+                }
+                else
+                {
+                    File.Create(jsonPath);
+                }
             }
 
             var jsonFormatting = Formatting.None;
@@ -29,39 +41,86 @@ namespace Unosquare.Labs.EmbedIO.JsonServer
 
                 if (path.StartsWith(basePath) == false) return false;
 
-                var parts = path.Substring(basePath.Length).Trim().Split('/');
-                if (String.IsNullOrWhiteSpace(parts[0]))
+                if (path == basePath)
                 {
-                    context.JsonResponse((string)JsonConvert.SerializeObject(Data, jsonFormatting));
+                    context.JsonResponse((string) JsonConvert.SerializeObject(Data, jsonFormatting));
                     return true;
                 }
 
+                var parts = path.Substring(basePath.Length).Split(new[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+
                 dynamic table;
 
-                switch (parts.Length)
+                if (parts.Length == 1)
                 {
-                    case 1:
-                        table = Data[parts[0]];
-                        if (table == null) return false;
+                    table = Data[parts[0]];
+                    if (table == null) return false;
+
+                    if (verb == HttpVerbs.Get)
+                    {
                         context.JsonResponse((string) JsonConvert.SerializeObject(table, jsonFormatting));
                         return true;
-                    case 2:
-                        table = Data[parts[0]];
-                        if (table == null) return false;
+                    }
 
-                        foreach (dynamic row in table)
+                    if (verb == HttpVerbs.Post)
+                    {
+                        var array = (JArray) table;
+                        array.Add(JsonConvert.DeserializeObject(context.RequestBody()));
+                        ThreadPool.QueueUserWorkItem(UpdateDataStore);
+
+                        return true;
+                    }
+                }
+
+                if (parts.Length == 2)
+                {
+                    table = Data[parts[0]];
+                    if (table == null) return false;
+
+                    foreach (dynamic row in table)
+                    {
+                        if (row["id"] != parts[1]) continue;
+
+                        if (verb == HttpVerbs.Get)
                         {
-                            if (row["id"] == parts[1])
-                            {
-                                context.JsonResponse((string) JsonConvert.SerializeObject(row, jsonFormatting));
-                                return true;
-                            }
+                            context.JsonResponse((string) JsonConvert.SerializeObject(row, jsonFormatting));
+                            return true;
                         }
-                        break;
+
+                        if (verb == HttpVerbs.Put)
+                        {
+                            var update = JsonConvert.DeserializeObject<JObject>(context.RequestBody());
+
+                            foreach (KeyValuePair<string, JToken> property in update)
+                            {
+                                row[property.Key] = property.Value;
+                            }
+
+                            ThreadPool.QueueUserWorkItem(UpdateDataStore);
+
+                            return true;
+                        }
+
+                        if (verb == HttpVerbs.Delete)
+                        {
+                            var array = (JArray)table;
+                            array.Remove(row);
+                            ThreadPool.QueueUserWorkItem(UpdateDataStore);
+
+                            return true;
+                        }
+                    }
                 }
 
                 return false;
             });
+        }
+
+        public void UpdateDataStore(object state)
+        {
+            if (String.IsNullOrWhiteSpace(JsonPath)) return;
+
+            File.WriteAllText(JsonPath, JsonConvert.SerializeObject(Data, Formatting.Indented));
         }
 
         public override string Name
