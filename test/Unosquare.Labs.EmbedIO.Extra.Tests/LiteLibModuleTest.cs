bf30a7c21f6adc3df5444cc7f7b2b18b084a01cf
chubbsnes65 <nestor.soto@unosquare.com>
2017-03-17 14:29:10 -0600

    Add LiteLibeModule tests(incomplete)

---
+++ test/Unosquare.Labs.EmbedIO.Extra.Tests/LiteLibModuleTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Unosquare.Labs.EmbedIO.Extra.Tests.TestObjects;
+using Unosquare.Labs.EmbedIO.LiteLibWebApi;
+using Unosquare.Swan.Formatters;
+using Unosquare.Swan.Networking;
+
+namespace Unosquare.Labs.EmbedIO.Extra.Tests
+{
+    [TestFixture]
+    public class LiteLibModuleTest
+    {
+        protected string RootPath;
+        protected string ApiPath = "dbapi";
+        protected string WebServerUrl;
+        protected WebServer WebServer;
+
+        [SetUp]
+        public void Init()
+        {
+            RootPath = TestHelper.SetupStaticFolder();
+
+            WebServerUrl = Resources.GetServerAddress();
+            WebServer = new WebServer(WebServerUrl);
+            WebServer.RegisterModule(new LiteLibModule<TestDbContext>(new TestDbContext(), ApiPath));
+            WebServer.RunAsync();
+        }
+
+        //Get All Orders
+        [Test]
+        public async Task GetAll()
+        {
+            var response = await JsonClient.GetString(WebServerUrl + ApiPath + "/order");
+            Assert.AreEqual(response, HttpStatusCode.OK, "Status Code OK");
+        }
+
+        [Test]
+        public async Task GetFirstItem()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(WebServerUrl + ApiPath + "/order/1");
+
+            using (var response = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.OK, "Status Code OK");
+
+                var jsonString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Assert.IsNotEmpty(jsonString);
+
+                dynamic json = Json.Deserialize(jsonString);
+                Assert.IsNotNull(json);
+            }
+        }
+
+        [Test]
+        public async Task Post()
+        {
+
+        }
+    }
+}
---
+++ test/Unosquare.Labs.EmbedIO.Extra.Tests/TestObjects/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Unosquare.Labs.LiteLib;
+
+namespace Unosquare.Labs.EmbedIO.Extra.Tests.TestObjects
+{
+    public class Order : LiteModel
+    {
+        [LiteUnique]
+        public string UniqueId { get; set; }
+
+        [LiteIndex]
+        public string CustomerName { get; set; }
+
+        [StringLength(30)]
+        public string ShipperCity { get; set; }
+
+        public bool IsShipped { get; set; }
+        public int Amount { get; set; }
+        public string ShippedDate { get; set; }
+
+    }
+
+    internal class TestDbContext : LiteDbContext
+    {
+        public TestDbContext(string name = "testDb") : base(Path.Combine(Path.GetTempPath(), $"{name}.db"))
+        {
+            if (Orders.Count() != 0) return;
+
+            Orders.Insert(new Order() { UniqueId = "1", CustomerName = "Unosquare" });
+            Orders.Insert(new Order() { UniqueId = "2", CustomerName = "Apple" });
+            Orders.Insert(new Order() { UniqueId = "3", CustomerName = "Microsoft" });
+            Orders.Insert(new Order() { UniqueId = "4", CustomerName = "Unosquare" });
+            Orders.Insert(new Order() { UniqueId = "5", CustomerName = "Unosquare" });
+        }
+
+        public LiteDbSet<Order> Orders { get; set; }
+    }
+}
