a03790f092d1899b46be9be1c43ea04cc5040170
Israel <arnulfo_israel@hotmail.com>
2017-09-14 14:17:46 -0500

    dotnet to 1.1.0 in travis and fixed GetIndex test

--- test/Unosquare.Labs.EmbedIO.Extra.Tests/TestObjects/Resources.cs
+++ test/Unosquare.Labs.EmbedIO.Extra.Tests/TestObjects/Resources.cs
@@ -20,8 +20,6 @@ namespace Unosquare.Labs.EmbedIO.Extra.Tests.TestObjects
 <li>Because bacon.</li>
 </ul>
 <p>If you are using testing Bearer Tokens, try to access to <a href=""/secure.html"">secure page</a>.</p>
-<p><a href=""http://unosquare.github.io/embedio"">Visit EmbedIO</a>.</p>
-
-<p><a href=""/api/"">Check JSON API</a></p>";
+<p><a href=""http://unosquare.github.io/embedio"">Visita EmbedIO</a>.</p>";
     }
 }
