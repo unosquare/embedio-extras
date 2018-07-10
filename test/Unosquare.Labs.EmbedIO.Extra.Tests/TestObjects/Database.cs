623568183b4389b6d8e0eb255076088e719799f8
Israel <arnulfo_israel@hotmail.com>
2018-07-10 13:51:11 -0500

    UniqueId removed

--- test/Unosquare.Labs.EmbedIO.Extra.Tests/TestObjects/Database.cs
+++ test/Unosquare.Labs.EmbedIO.Extra.Tests/TestObjects/Database.cs
@@ -5,9 +5,6 @@
 
     internal class Order : LiteModel
     {
-        [LiteUnique]
-        public string UniqueId { get; set; }
-
         [LiteIndex]
         public string CustomerName { get; set; }
         
@@ -25,11 +22,11 @@
         {
             if (Orders.Count() != 0) return;
 
-            Orders.Insert(new Order() { UniqueId = "1", CustomerName = "Unosquare" });
-            Orders.Insert(new Order() { UniqueId = "2", CustomerName = "Apple" });
-            Orders.Insert(new Order() { UniqueId = "3", CustomerName = "Microsoft" });
-            Orders.Insert(new Order() { UniqueId = "4", CustomerName = "Unosquare" });
-            Orders.Insert(new Order() { UniqueId = "5", CustomerName = "Unosquare" });
+            Orders.Insert(new Order() { CustomerName = "Unosquare" });
+            Orders.Insert(new Order() { CustomerName = "Apple" });
+            Orders.Insert(new Order() { CustomerName = "Microsoft" });
+            Orders.Insert(new Order() { CustomerName = "Unosquare" });
+            Orders.Insert(new Order() { CustomerName = "Unosquare" });
         }
 
         public LiteDbSet<Order> Orders { get; set; }
