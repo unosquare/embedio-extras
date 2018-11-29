469696074fda05ec07e2a38fe72abc68c49385a1
Geo Perez <geovanni.perez@gmail.com>
2018-11-29 13:47:55 -0600

    Add new projects

---
+++ src/Unosquare.EmbedIO.AspNetCore.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unosquare.EmbedIO.AspNetCore.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Unosquare.EmbedIO.AspNetCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Unosquare.EmbedIO.AspNetCore
+{
+    public class Class1
+    {
+    }
+}
