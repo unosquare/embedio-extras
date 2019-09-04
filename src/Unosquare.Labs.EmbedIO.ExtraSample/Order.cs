3b06f7490fe1b7fa1d77cc017b357da583d4e58f
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-09-04 09:34:16 -0500

    Update EmbedIO

---
+++ src/Unosquare.Labs.EmbedIO.ExtraSample/Order.cs
@@ -0,0 +1,22 @@
+﻿using Unosquare.Labs.LiteLib;
+
+namespace EmbedIO.ExtraSample
+{
+    internal class Order : LiteModel
+    {
+        [LiteUnique]
+        public string UniqueId { get; set; }
+
+        [LiteIndex]
+        public string CustomerName { get; set; }
+
+        [StringLength(30)]
+        public string ShipperCity { get; set; }
+
+        public bool IsShipped { get; set; }
+
+        public int Amount { get; set; }
+
+        public string ShippedDate { get; set; }
+    }
+}
--- src/Unosquare.Labs.EmbedIO.ExtraSample/TestDbContext.cs
+++ src/Unosquare.Labs.EmbedIO.ExtraSample/TestDbContext.cs
@@ -3,24 +3,6 @@
     using System.IO;
     using Unosquare.Labs.LiteLib;
 
-    internal class Order : LiteModel
-    {
-        [LiteUnique]
-        public string UniqueId { get; set; }
-
-        [LiteIndex]
-        public string CustomerName { get; set; }
-
-        [StringLength(30)]
-        public string ShipperCity { get; set; }
-
-        public bool IsShipped { get; set; }
-
-        public int Amount { get; set; }
-
-        public string ShippedDate { get; set; }
-    }
-
     internal class TestDbContext : LiteDbContext
     {
         public TestDbContext(string name = "testDb") 
